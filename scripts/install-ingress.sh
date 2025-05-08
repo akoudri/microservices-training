8b3c6bf926f1a360ff1e6286c5bdca5ff9736645
akoudri <ali.koudri@gmail.com>
2025-05-08 14:33:01 +0200

    Completes ingress configuration for path mode

--- scripts/install-ingress.sh
+++ scripts/install-ingress.sh
@@ -4,9 +4,5 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 kubectl create namespace ingress-nginx
 helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
-  --namespace ingress-nginx \
-  --set controller.ingressClassResource.name=nginx \
-  --set controller.service.loadBalancerIP=34.79.219.176
+  --namespace ingress-nginx 
 
-# FIXME: solution temporaire pour le problème de webhook admission
-kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission

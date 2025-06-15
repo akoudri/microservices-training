e39cb1c43f4765f85d9fe97ac60e39cbe8b89b97
akoudri <ali.koudri@gmail.com>
2025-06-15 21:59:14 +0200

    Completes supervision examples

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
-microk8s.kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
 
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+  --namespace kubernetes-dashboard \
+  --values values.yaml
+
+# kubectl create serviceaccount admin-user -n kubernetes-dashboard
+
+# kubectl create clusterrolebinding admin-user \
+#   --clusterrole=cluster-admin \
+#   --serviceaccount=kubernetes-dashboard:admin-user
+
+# kubectl -n kubernetes-dashboard create token admin-user
+
+# kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard 8443:443

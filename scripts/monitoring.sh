d61012b2c0bb74c9863d393e4e3293d5dd7308c8
akoudri <ali.koudri@gmail.com>
2025-06-29 10:39:34 +0200

    Refactors monitoring

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+
 helm repo update
+
 
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
   --namespace observability --create-namespace
+
+helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator --namespace observability
+
+kubectl apply -f telemetry.yaml
 
 # kubectl get secret --namespace observability prometheus-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
--- scripts/telemetry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm repo update
-
-helm install opentelemetry-operator open-telemetry/opentelemetry-operator --namespace observability
-
-kubectl apply -f telemetry.yaml
